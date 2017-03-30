382bef97ce4f8c58d5ac3334751aff29ddbc3707
guimaodai <wyyxdgm@163.com>
2017-03-30 09:55:54 +0800

    server clean

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 process.on('uncaughtException', function(err) {
-    console.log('-----------------------')
-    console.log(err.stack);
-    console.log('-----------------------')
+	console.log('-----------------------')
+	console.log(err.stack);
+	console.log('-----------------------')
 });
 
-var fs = require('fs');
-var path = require('path');
 var app = require('./app');
-
-if (!fs.existsSync('config.js')) {
-    console.log('no config.js');
-    process.exit(0);
-}
-
 require('http').createServer(app).listen(app.get('port'), function(err) {
-    console.log('Express server listening on port: ' + app.get('port'));
+	console.log('Express server listening on port: ' + app.get('port'));
 });
